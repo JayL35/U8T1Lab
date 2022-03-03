4c08a7029bee73448a617c336e81e30e3dd18388
BT_1E10_23 <sample@bths.edu>
2022-03-03 13:14:37 -0500

    Final

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -13,5 +13,37 @@ public class FunWith2DArrays
         System.out.println(strings[strings.length - 1][strings[0].length - 1]);
     }
 
+    public static double average(int[][] ints)
+    {
+        double sum = 0;
+        for (int row = 0; row < ints.length; row++)
+        {
+            for (int col = 0; col < ints[0].length; col++)
+            {
+                sum += ints[row][col];
+            }
+        }
+        return sum / (double) (ints.length * ints[0].length);
+    }
+
+    public static int[] indexFound(String[][] words, String target)
+    {
+        int[] result = {-1, -1};
+
+        for (int row = 0; row < words.length; row++)
+        {
+            for (int col = 0; col < words[0].length; col++)
+            {
+                if (words[row][col].equals(target))
+                {
+                    result[0] = row;
+                    result[1] = col;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
 
 }
